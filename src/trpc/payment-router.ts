056219ffb9b946fd8cd8793e50d3526b9b1e13f7
mouhamed anflouss <mouhamedanflosse@gmail.com>
2024-01-29 23:25:14 +0100

    enhance

--- src/trpc/payment-router.ts
+++ src/trpc/payment-router.ts
@@ -59,7 +59,7 @@ export const paymentRouter = router({
         const stripeSession = await stripe.checkout.sessions.create({
           success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderID=${order.id}`,
           cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/cart`,
-          payment_method_types: ["card", "paypal"],
+          payment_method_types: ["card", "cashapp"],
           mode: "payment",
           metadata: {
             userId: user.id,
